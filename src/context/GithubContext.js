f6268295d889c2aaa1ecd7b1c115151093867606
Shritej Mayekar <shritejmayekar@gmail.com>
2022-12-05 23:17:36 +0530

    [shritej] Added context api with app

---
+++ src/context/GithubContext.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+import React from "react";
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REAT_APP_GITHUB_TOKEN
+
+
+export const GithubProvider = ({children}) => {
+    const [users,setUsers] = useState([])
+    const [loading,setLoading] = useState(true)
+    const fetchUser = async () => {
+        const response = await fetch(`${GITHUB_URL}/users`,{
+            headers :{
+                Authorization:`${GITHUB_TOKEN}`
+            }
+        })
+        const data = await response.json()
+        setUsers(data)
+        setLoading(false)
+    }
+    return <GithubContext.Provider  value={{
+        users,
+        loading,
+        fetchUser
+    }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export default GithubContext
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+        {/* user result components   */}
+        <UserResults />
+    </>
+  )
+}
+
+export default Home
